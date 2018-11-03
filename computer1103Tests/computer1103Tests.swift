29e93fcef433143dd7c623d35e064876ec3feff4
ZhaiDongbobug <18747867831@163.com>
2018-11-03 08:35:49 +0800

    Initial Commit

---
+++ computer1103Tests/computer1103Tests.swift
@@ -0,0 +1,34 @@
+//
+//  computer1103Tests.swift
+//  computer1103Tests
+//
+//  Created by ss20171103190 on 2018/11/3.
+//  Copyright © 2018 ss20171103190. All rights reserved.
+//
+
+import XCTest
+@testable import computer1103
+
+class computer1103Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
